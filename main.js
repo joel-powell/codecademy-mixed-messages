9fb39875bee955370e1cc3ba424acf7156bf9457
Joel Powell <joelhpowell@icloud.com>
2021-11-16 15:41:57 +0000

    Consolidate arrays

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const pronouns = ["I", "We", "You", "He", "She", "They"]
-const actions = ["lived in", "went on holiday to", "have always wanted to go to", "met someone in"]
-const places = ["London", "Tokyo", "Paris", "Rome", "Berlin", "Bangkok"]
-const arrays = [pronouns, actions, places]
+const arrays = [
+    ["I", "We", "You", "He", "She", "They"], 
+    ["lived in", "went on holiday to", "have always wanted to go to", "met someone in"], 
+    ["London", "Tokyo", "Paris", "Rome", "Berlin", "Bangkok"]
+]
 
 console.log(arrays.map((element, index) => element[Math.floor(Math.random() * element.length)]).join(" "));
